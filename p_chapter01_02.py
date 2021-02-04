c61a92576ae12dd766655a9bc902deadbe9e3ebf
chamyeongseop <chamysub@postech.ac.kr>
2021-02-05 02:42:51 +0900

    Study

--- p_chapter01_02.py
+++ p_chapter01_02.py
@@ -1,1 +1,11 @@
+import pendulum
+from datetime import datetime
 
+pst = pendulum.timezone('America/Los_Angeles')
+ist = pendulum.timezone(('Asia/Seoul'))
+
+print(type(pst))
+
+# 타임존 시간 출력
+print('Current Date Time in PST = ', datetime.now(pst))
+print('Current Date Time in IST = ', datetime.now(ist))
