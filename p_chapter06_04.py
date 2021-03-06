3599aab638aaf45855a7f13dfc48ff37aa14cc95
chamyeongseop <chamysub@postech.ac.kr>
2021-03-06 16:08:24 +0900

    Finish

--- p_chapter06_04.py
+++ p_chapter06_04.py
@@ -1,19 +1,22 @@
 # Chapter06-04
 # Futures 동시성
+# 파이썬 3.2 버전부터 새롭게 등장
 # 비동기 작업 실행
 # 지연시간(Block) CPU 및 리소스 낭비 방지 -> (File)Network I/O 관련 작업 -> 동시성 활용 권장
 # 비동기 작업과 적합한 프로그램일 경우 압도적으로 성능 향상
 
-# futures : 비동기 실행을 위한 API를 고수준으로 작성 -> 사용하기 쉽도록 개선
+# futures 패키지: 비동기 실행을 위한 API를 고수준으로 작성 -> 사용하기 쉽도록 개선
+# 1. futures 내부에 Threading과 Multiprocessing 패키지들이 모두 포함되어 있다.
+
 # concurrent.Futures
 # 1. 멀티스레딩/멀티프로세싱 API 통일 -> 매우 사용하기 쉬움
-# 2. 실행중이 작업 취소, 완료 여부 체크, 타임아웃 옵션, 콜백추가, 동기화 코드 매우 쉽게 작성 -> Promise 개념
+# 2. 실행중이 작업 취소, 완료 여부 체크, 타임아웃 옵션, 콜백 함수 추가, 동기화 코드도 매우 쉽게 작성 -> Promise 개념
 
 # 2가지 패턴 실습
 # concurrent.futures 사용법1
 # concurrent.futures 사용법2
 
-# GIL : 두 개 이상의 스레드가 동시에 실행 될 때 하나의 자원을 엑세스 하는 경우 -> 문제점을 방지하기 위해
+# GIL(Global Interpreter Lock) : 두 개 이상의 스레드가 동시에 실행 될 때 하나의 자원을 엑세스 하는 경우 -> 문제점을 방지하기 위해
 #       GIL 실행 , 리소스 전체에 락이 걸린다. -> Context Switch(문맥 교환)
 
 # GIL : 멀티프로세싱 사용, CPython
@@ -47,5 +50,6 @@ def main():
     print(msg.format(list(result), end_tm))
 
 # 실행
+# Main함수의 진입점을 알려줌. -> 첫 시작점을 알려줘야 함(알려주지 않으면, Multi-processing을 수행할 수 없음)
 if __name__ == '__main__':
     main()
