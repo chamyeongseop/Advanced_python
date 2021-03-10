53eaaef8c5582f464928d1fcacc377f72539b6e7
chamyeongseop <chamysub@postech.ac.kr>
2021-03-11 00:51:55 +0900

    Finish Class

--- p_chapter07_02.py
+++ p_chapter07_02.py
@@ -17,6 +17,7 @@ from bs4 import BeautifulSoup
 
 import sys
 import io
+# 한글 깨짐 방지
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
